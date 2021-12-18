8188a201feefbc4842d8477a43753282ff426104
NotPeOpLe <zombieasura@outlook.com>
2021-12-18 17:04:09 +0800

    + __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .client import IrcClient
+from .objects.channel import Channel
+from .objects.message import Message
+from .objects.osuenums import GameMode, ScoreMode, TeamMode, Mods
+from .objects.slot import Slot, Slots
