b4634679e75f3a04792da1209978bb353bac3624
NotPeOpLe <zombieasura@outlook.com>
2021-12-20 14:42:07 +0800

    變變變，變成套件

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="osuirc", # 
+    version="0.0.1",
+    author="Code840",
+    author_email="code840@outlook.com",
+    description="專門給osu使用的簡易客戶端",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NotPeOpLe/osuirc",
+    packages=["osuirc", "osuirc.utils", "osuirc.objects"],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
